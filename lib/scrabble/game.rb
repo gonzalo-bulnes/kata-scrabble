a7bb5dae1ffe0502a3f3744e96c1db5f7fc94e10
Gonzalo Bulnes Guilpain <gon.bulnes@gmail.com>
2015-11-11 10:42:07 -0500

    Fix Game to reflect the game state
    I'm trying to follow the recomendations provided by
    the How to Design Programs - 2nd Edition (HTDP2e) book.
    The recipe is meant to be used to design programs in a functional style,
    but I want to explore to which extent the HTDP2e recommendations
    can improve my object-oriented design skills.
    
    Scrabble is an interactive program (a world program in the HTDP2e
    vocabulary.) According to HTDP2e, the first step in the design of
    such programs consists in defining a data type to hold the state
    of the program. The structure of the data will then shape the
    structure of the program. For us, `Game` is that structure.
    
    See http://www.ccs.neu.edu/home/matthias/HtDP2e and
    particularly the end of the 6.1 section for the design recipe.

--- lib/scrabble/game.rb
+++ lib/scrabble/game.rb
@@ -16,8 +16,37 @@ module Scrabble
 
     private
 
+      # Returns a Board
       def board
         @board ||= Board.new
+      end
+
+      # Returns an Array of Players
+      def players
+        @players ||= [Player.new('Alice'), Player.new('Bob'), Player.new('Charles'), Player.new('Dani')]
+      end
+
+      # Returns a Player
+      def current_player
+        players[0]
+      end
+
+      # Returns an Array of Rules
+      def rules
+        @rules ||= [Rule.new('Tiles cannot be placed out of the board.')]
+      end
+
+      # Returns an Array of Tiles
+      def remaining_tiles
+        @remaining_tiles ||= Configuration.default_tiles_bag
+      end
+      alias :bag :remaining_tiles
+
+      # How many times in a row did players pass
+      #
+      # Returns a Fixnum
+      def successive_pass_count
+        @successive_pass_count ||= 0
       end
 
   end
--- spec/lib/scrabble/game_spec.rb
+++ spec/lib/scrabble/game_spec.rb
@@ -17,6 +17,64 @@ module Scrabble
         game.run
       end
     end
+
+    describe '#board', private: true do
+
+      it 'returns a empty board' do
+        expect(game.send(:board)).to be_kind_of Board
+      end
+    end
+
+    describe '#players', private: true do
+
+      it 'returns an array of players' do
+        pending
+        expect(game.send(:players)).to be_kind_of Array
+        expect(game.send(:players)).not_to be_empty
+        expect(game.send(:players)[0]).to be_kind_of Player
+      end
+    end
+
+    describe '#current_player', private: true do
+
+      it 'returns the current_player' do
+        pending
+        expect(game.send(:current_player)).to be_kind_of Player
+      end
+    end
+
+    describe '#rules', private: true do
+
+      it 'returns an array of rules' do
+        pending
+        expect(game.send(:rules)).to be_kind_of Array
+        expect(game.send(:rules)).not_to be_empty
+        expect(game.send(:rules)[0]).to be_kind_of Rule
+      end
+    end
+
+    describe '#remaining_tiles', private: true do
+
+      it 'returns an array of tiles' do
+        pending
+        expect(game.send(:remaining_tiles)).to be_kind_of Array
+        expect(game.send(:remaining_tiles)).not_to be_empty
+        expect(game.send(:remaining_tiles)[0]).to be_kind_of Tile
+      end
+    end
+
+    describe '#successive_pass_count', private: true do
+
+      it 'gets incremented when a player passes' do
+        pending
+        expect().to change(successive_pass_count).by(1)
+      end
+
+      it 'gets reset when a player does not pass' do
+        pending
+        expect().to change(successive_pass_count).to(0)
+      end
+    end
   end
 end
 
